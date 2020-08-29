4af9106f759a4a926de56b97238a31ab66484659
Tushit Garg <tushit.23@gmail.com>
2020-08-29 20:57:31 +0530

    API to delete a particular ticket added.

--- api.py
+++ api.py
@@ -7,29 +7,6 @@ app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 con = sqlite3.connect('database.db', check_same_thread=False)
 
-#API to  view tickets of a particular time
-@app.route('/api/showtickets', methods=['GET'])
-def show_rows():
-	query_parameters = request.args
-	timing = query_parameters.get("timing")
-	if timing :
-		time = timing
-		cursorObj = con.cursor()
-		cursorObj.execute("SELECT * from tickets where timings=?",[time])
-		rows = cursorObj.fetchall()
-		results = []
-		for row in rows:
-			#print(,' ','username',row[1],' ','phonenumber',row[2],' ','timing',row[3])
-			dic = {}
-			dic["ticketid"] = row[0]
-			dic["User's name"] = row[1]
-			dic["Phone Number"] = row[2]
-			dic["Timing"] = row[3]
-			results.append(dic)
-		return jsonify(results)
-	else:
-		return "<h1>Error!!</h1><p>Time parameter is not provided.</p>"
-
 # API to book a ticket using a user’s name, phone number, and timings.
 @app.route('/api/bookticket', methods=['GET'])
 def insert_row():
@@ -74,6 +51,46 @@ def updateTicket():
 		return "<h1>Your ticket timing is updated to {}</h1><p>Thank you !</p>".format(time)
 	except:
 		print("update failed")
+		return "<h1>Some error occured!</h1>"
+
+#API to  view tickets of a particular time
+@app.route('/api/showtickets', methods=['GET'])
+def show_rows():
+	query_parameters = request.args
+	timing = query_parameters.get("timing")
+	if timing :
+		time = timing
+		cursorObj = con.cursor()
+		cursorObj.execute("SELECT * from tickets where timings=?",[time])
+		rows = cursorObj.fetchall()
+		results = []
+		for row in rows:
+			#print(,' ','username',row[1],' ','phonenumber',row[2],' ','timing',row[3])
+			dic = {}
+			dic["ticketid"] = row[0]
+			dic["User's name"] = row[1]
+			dic["Phone Number"] = row[2]
+			dic["Timing"] = row[3]
+			results.append(dic)
+		return jsonify(results)
+	else:
+		return "<h1>Error!!</h1><p>Time parameter is not provided.</p>"
+
+# API to delete a particular ticket
+@app.route('/api/deleteticket', methods=['GET'])
+def deleteTicket():
+	query_parameters = request.args
+	id_ = query_parameters.get("id")
+	if not (id_ or timing):
+		return "<h1>Error!!</h1><p>Some parameter's missing. Please pass the ticket id.</p>"
+	cursorObj = con.cursor()
+	try:
+		cursorObj.execute("DELETE FROM tickets where id = ?",(id_,))
+		con.commit()
+		print("delete successfully done")
+		return "<h1>Ticket with id {} id deleted</h1><p>Thank you !</p>".format(id_)
+	except:
+		print("delete failed")
 		return "<h1>Some error occured!</h1>"
 if __name__ == '__main__':
 	app.run()
